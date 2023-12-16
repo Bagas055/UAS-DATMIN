6a735186b472773986de8e886ed82e58fe8aac71
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-16 22:27:47 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 model = pickle.load(open('air_passenger_forecasting.sav','rb'))
 
