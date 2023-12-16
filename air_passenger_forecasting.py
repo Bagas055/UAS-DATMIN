0388ff4662caa28c130fe29c97cd65fa4b6e8176
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-16 22:39:14 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -2,13 +2,15 @@ import pickle
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
 
 model = pickle.load(open('air_passenger_forecasting.sav','rb'))
 
 df = pd.read_csv('AirPassengers.csv')
-df['Month']=pd.to_datetime(df['Month'], format='%Y-%m')
+df['Month']=pd.to_datetime(df['Month'], format='%Y-%m-%d')
 df.set_index(['Month'], inplace=True)
+
+st.title('Forecasting Air Passenger')
+year = st.slider("Tentukan Tahun",1,30, step=1)
 
 # buat DataFrame untuk data yang stasioner
 df_stationary = df.copy()
@@ -24,14 +26,9 @@ del df_stationary ['Passengers']
 del df_stationary ['Passengers_Stationary']
 df_stationary.head()
 
-st.title('Forecasting Air Passenger')
-year = st.slider("Tentukan Tahun",1,30, step=1)
 
 pred = model.forecast(year)
 pred = pd.DataFrame(pred, columns=['Passengers_Stationary_2'])
-
-ar = ARIMA(df_stationary, order=(15,1,15)).fit()
-ar_test_pred = ar.forecast(20)
 
 if st.button("Predict"):
 
@@ -41,5 +38,5 @@ if st.button("Predict"):
     with col2:
         fig, ax = plt.subplots()
         df_stationary['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
-        ar_test_pred.plot(color='b', legend=True, label='Prediction')
+        pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
         st.pyplot(fig)
