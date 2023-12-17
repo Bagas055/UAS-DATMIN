21e37a9ca0d0f22a0db328c74a6d0dfcff3653e4
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 07:59:30 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -3,34 +3,25 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
+model = pickle.load(open('air_passenger_forecasting.sav','rb'))
 
-# Ubah sesuai dengan dataset baru yang memiliki kolom 'Month' dan 'Passengers_Stationary_2'
 df = pd.read_csv("AirPassengers.csv")
-df['Month'] = pd.to_datetime(df['Month'])  # Pastikan kolom 'Month' memiliki tipe data datetime
-df.set_index(['Month'], inplace=True)
+df['Year'] = pd.to_datetime(df['Passengers_Stationary_2'], format='%Y')
+df.set_index(['Passengers_Stationary_2'], inplace=True)
 
-df['Passengers_Stationary_2'] = pd.to_numeric(df['Passengers_Stationary_2'], errors='coerce')
+st.title('Forecasting Air Passenger')
+year = st.slider("Tentukan Tahun",1,30, step=1)
 
-st.title('Forecasting Air Passengers')
-
-# Ubah slider menjadi memilih jumlah bulan untuk diprediksi
-months_to_predict = st.slider("Tentukan Jumlah Bulan untuk Diprediksi", 1, 30, step=1)
+pred = model.forecast(year)
+pred = pd.DataFrame(pred, columns=['CO2'])
 
 if st.button("Predict"):
 
-    # Lakukan prediksi menggunakan model yang telah diload
-    pred = model.forecast(steps=months_to_predict)
-    pred = pd.DataFrame(pred, columns=['Passengers_Stationary_2'])
-
-    # Tampilkan tabel hasil prediksi
-    st.subheader("Hasil Prediksi")
-    st.write(pred)
-
-    # Buat plot menggunakan Matplotlib dan tampilkan di Streamlit
-    fig, ax = plt.subplots()
-    df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='Known')
-    pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
-    plt.xlabel('Month')
-    plt.ylabel('Passengers_Stationary_2')
-    st.pyplot(fig)
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.dataframe(pred)
+    with col2:
+        fig, ax = plt.subplots()
+        df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
+        pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
+        st.pyplot(fig)
