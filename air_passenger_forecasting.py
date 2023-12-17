3181a28a851dea127a9f9a19d4b90cdc65b12e77
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:07:27 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -3,25 +3,29 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('air_passenger_forecasting.sav','rb'))
+model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
 
+# Ubah 'Month' menjadi tipe data datetime dan pastikan 'Passengers_Stationary_2' adalah numerik
 df = pd.read_csv("AirPassengers.csv")
-df['Year'] = pd.to_datetime(df['Month'], format='%Y-%m')
-df.set_index(['Month'], inplace=True)
+df['Month'] = pd.to_datetime(df['Month'])
+df['Passengers_Stationary_2'] = pd.to_numeric(df['Passengers_Stationary_2'], errors='coerce')
+df.set_index('Month', inplace=True)
 
 st.title('Forecasting Air Passenger')
-year = st.slider("Tentukan Tahun",1,30, step=1)
-
-pred = model.forecast(year)
-pred = pd.DataFrame(pred, columns=['Passengers_Stationary_2'])
+num_years = st.slider("Tentukan Jumlah Tahun untuk Forecast", 1, 10, step=1)
 
 if st.button("Predict"):
+    # Lakukan prediksi menggunakan model untuk periode yang dipilih
+    forecast = model.forecast(steps=num_years * 12)  # Ubah ke bulan jika model.forecast() membutuhkan periode bulanan
+    
+    # Buat DataFrame untuk hasil prediksi
+    pred_index = pd.date_range(start=df.index[-1], periods=num_years * 12 + 1, freq='M')[1:]
+    pred = pd.DataFrame(forecast, index=pred_index, columns=['Passengers_Stationary_2'])
 
-    col1, col2 = st.columns([2,3])
-    with col1:
-        st.dataframe(pred)
-    with col2:
-        fig, ax = plt.subplots()
-        df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
-        pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
-        st.pyplot(fig)        
+    # Plot hasil prediksi
+    fig, ax = plt.subplots()
+    df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
+    pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
+    plt.xlabel('Month')
+    plt.ylabel('Passengers_Stationary_2')
+    st.pyplot(fig)
