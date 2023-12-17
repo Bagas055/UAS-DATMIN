d89e971f6e1e5597eb3ccd56c1275faa8ec53c5c
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:08:47 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -5,27 +5,28 @@ import matplotlib.pyplot as plt
 
 model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
 
-# Ubah 'Month' menjadi tipe data datetime dan pastikan 'Passengers_Stationary_2' adalah numerik
+# Load dataset
 df = pd.read_csv("AirPassengers.csv")
 df['Month'] = pd.to_datetime(df['Month'])
-df['Passengers_Stationary_2'] = pd.to_numeric(df['Passengers_Stationary_2'], errors='coerce')
 df.set_index('Month', inplace=True)
 
 st.title('Forecasting Air Passenger')
 num_years = st.slider("Tentukan Jumlah Tahun untuk Forecast", 1, 10, step=1)
 
 if st.button("Predict"):
-    # Lakukan prediksi menggunakan model untuk periode yang dipilih
-    forecast = model.forecast(steps=num_years * 12)  # Ubah ke bulan jika model.forecast() membutuhkan periode bulanan
+    # Create new index for prediction based on the frequency of the original data
+    pred_index = pd.date_range(start=df.index[-1], periods=num_years * 12, freq='MS')  # MS: Month Start frequency
     
-    # Buat DataFrame untuk hasil prediksi
-    pred_index = pd.date_range(start=df.index[-1], periods=num_years * 12 + 1, freq='M')[1:]
+    # Perform forecast
+    forecast = model.forecast(steps=num_years * 12)
+    
+    # Create a new DataFrame for predictions with appropriate index
     pred = pd.DataFrame(forecast, index=pred_index, columns=['Passengers_Stationary_2'])
 
-    # Plot hasil prediksi
+    # Plotting known and predicted values
     fig, ax = plt.subplots()
-    df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
-    pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
+    df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='Known')
+    pred.plot(ax=ax, color='b', legend=True, label='Prediction')
     plt.xlabel('Month')
     plt.ylabel('Passengers_Stationary_2')
     st.pyplot(fig)
