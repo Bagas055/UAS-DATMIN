888d29ab6d79af6153f4e366a86360c00c23b16e
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:31:24 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -2,33 +2,34 @@ import pickle
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
 
-model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
+model = pickle.load(open('air_passenger_forecasting.sav','rb'))
 
-df = pd.read_csv("AirPassenger2.csv")
-df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
+df = pd.read_csv("AirPassengers.csv")
+df['Year'] = pd.to_datetime(df['Month'], format='%Y-%m')
 df.set_index(['Month'], inplace=True)
 
-st.title('Forecasting Kualitas Udara')
-num_periods = st.slider("Tentukan Jumlah Periode untuk Diprediksi", 1, 30, step=1)
+st.title('Forecasting Air Passenger')
+year = st.slider("Tentukan Tahun",1,30, step=1)
+
+ar = ARIMA(df_stationary, order=(15,1,15)).fit()
+ar_train_pred = ar.fittedvalues
+ar_test_pred = ar.forecast(year)
+
+train_df_stationary = df_stationary.loc[:'1949-03']
+test_df_stationary = df_stationary.loc['1949-04':]
+
+pred = model.forecast(year)
+pred = pd.DataFrame(pred, columns=['Passengers_Stationary_2'])
 
 if st.button("Predict"):
-    # Assuming 'Passengers_Stationary_2' is the column you want to forecast
-    endog_data = df['Passengers_Stationary_2']
 
-    # Fit ARIMA model
-    arima_model = ARIMA(endog_data, order=(15, 1, 15))
-    arima_result = arima_model.fit()
-
-    # Make ARIMA forecast
-    arima_forecast = arima_result.forecast(steps=num_periods)
-
-    # Plotting known and predicted values
-    fig, ax = plt.subplots()
-    endog_data.plot(style='--', color='b', legend=True, label='Known')
-    ax.plot(arima_forecast.index, arima_forecast.values, color='red', label='Prediction')
-    plt.legend()
-    plt.xlabel('Time')
-    plt.ylabel('Passengers_Stationary_2')
-    st.pyplot(fig)
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.dataframe(pred)
+    with col2:
+        fig, ax = plt.subplots()
+        train_df_stationary['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='train_df')
+        test_df_stationary['Passengers_Stationary_2'].plot(style='--', color='r', legend=True, label='test_df')
+        ar_test_pred.plot(color='b', legend=True, label='Prediction')
+        st.pyplot(fig)
