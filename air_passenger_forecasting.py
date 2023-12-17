f810e906026374453bc80c0a7aae864acd219e09
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:36:32 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -4,34 +4,40 @@ import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('air_passenger_forecasting.sav','rb'))
+# Load the ARIMA model
+model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
 
+# Load the dataset
 df = pd.read_csv("AirPassengers.csv")
-df['Year'] = pd.to_datetime(df['Month'], format='%Y-%m')
+df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
 df.set_index(['Month'], inplace=True)
-df['Passengers_Stationary_2'] = pd.to_numeric(df['Passengers_Stationary_2'], errors='coerce')
 
 st.title('Forecasting Air Passenger')
-year = st.slider("Tentukan Tahun",1,30, step=1)
+year = st.slider("Tentukan Tahun", 1, 30, step=1)
 
-ar = ARIMA(df, order=(15,1,15)).fit()
+# Fit ARIMA model
+ar = ARIMA(df, order=(15, 1, 15)).fit()
 ar_train_pred = ar.fittedvalues
-ar_test_pred = ar.forecast(year)
 
+# Forecast with ARIMA
+ar_test_pred = ar.forecast(steps=year)
+
+# Convert 'Passengers_Stationary_2' column to numeric
+df['Passengers_Stationary_2'] = pd.to_numeric(df['Passengers_Stationary_2'], errors='coerce')
+
+# Split the data into train and test sets
 train_df = df.loc[:'1949-03']
 test_df = df.loc['1949-04':]
 
-pred = model.forecast(year)
-pred = pd.DataFrame(pred, columns=['Passengers_Stationary_2'])
-
 if st.button("Predict"):
-
-    col1, col2 = st.columns([2,3])
+    col1, col2 = st.columns([2, 3])
     with col1:
-        st.dataframe(pred)
+        st.dataframe(ar_test_pred)
     with col2:
         fig, ax = plt.subplots()
         train_df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='train_df')
         test_df['Passengers_Stationary_2'].plot(style='--', color='r', legend=True, label='test_df')
         ar_test_pred.plot(color='b', legend=True, label='Prediction')
+        plt.xlabel('Month')
+        plt.ylabel('Passengers_Stationary_2')
         st.pyplot(fig)
