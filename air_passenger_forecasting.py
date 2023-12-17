f6c4535015cb81a2d4bb52ab32033d4b04960086
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:16:48 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -3,33 +3,25 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
+model = pickle.load(open('air_passenger_forecasting.sav','rb'))
 
-# Load dataset
-df = pd.read_csv("AirPassengers.csv")
-df['Month'] = pd.to_datetime(df['Month'])
-df.set_index('Month', inplace=True)
+df = pd.read_csv("AirPassenger2.csv")
+df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m')
+df.set_index(['month'], inplace=True)
 
-st.title('Forecasting Air Passenger')
-num_years = st.slider("Tentukan Jumlah Tahun untuk Forecast", 1, 10, step=1)
+st.title('Forecasting Kualitas Udara')
+year = st.slider("Tentukan Tahun",1,30, step=1)
+
+ar = ARIMA(df_stationary, order=(15,1,15)).fit()
+ar_test_pred = ar.forecast(year)
 
 if st.button("Predict"):
-    # Create new index for prediction based on the frequency of the original data
-    pred_index = pd.date_range(start=df.index[-1], periods=num_years * 12, freq='MS')  # MS: Month Start frequency
-    
-    # Perform forecast
-    forecast = model.forecast(steps=num_years * 12)
-    
-    # Create a new DataFrame for predictions with appropriate index
-    pred = pd.DataFrame(forecast, index=pred_index, columns=['Passengers_Stationary_2'])
 
-    # Combine original data and predictions for plotting
-    combined_data = pd.concat([df, pred])
-
-    # Plotting known and predicted values
-    fig, ax = plt.subplots()
-    combined_data['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='Known')
-    pred.plot(ax=ax, color='b', legend=True, label='Prediction')
-    plt.xlabel('Month')
-    plt.ylabel('Passengers_Stationary_2')
-    st.pyplot(fig)
+    col1, col2 = st.columns([2,3])
+    with col1:
+        st.dataframe(ar)
+    with col2:
+        fig, ax = plt.subplots()
+        df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='known')
+        pred['Passengers_Stationary_2'].plot(color='b', legend=True, label='Prediction')
+        st.pyplot(fig)
