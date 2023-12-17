2952ef20f6ad896b1412159d5808f57f3ee2911f
Bagas055 <115714284+Bagas055@users.noreply.github.com>
2023-12-17 08:11:14 +0700

    Update air_passenger_forecasting.py

--- air_passenger_forecasting.py
+++ air_passenger_forecasting.py
@@ -1,32 +1,26 @@
-import pickle
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
 
-model = pickle.load(open('air_passenger_forecasting.sav', 'rb'))
+# Load the stationary dataset (df_stationary)
+df_stationary = pd.read_csv("your_stationary_dataset.csv")
+# Assuming 'Passengers_Stationary_2' is the column containing the stationary data
 
-# Load dataset
-df = pd.read_csv("AirPassengers.csv")
-df['Month'] = pd.to_datetime(df['Month'])
-df.set_index('Month', inplace=True)
+st.title('ARIMA Forecasting')
 
-st.title('Forecasting Air Passenger')
-num_years = st.slider("Tentukan Jumlah Tahun untuk Forecast", 1, 10, step=1)
+if st.button("Predict ARIMA"):
+    # Fit ARIMA model
+    ar = ARIMA(df_stationary, order=(15, 1, 15)).fit()
 
-if st.button("Predict"):
-    # Create new index for prediction based on the frequency of the original data
-    pred_index = pd.date_range(start=df.index[-1], periods=num_years * 12, freq='MS')  # MS: Month Start frequency
-    
-    # Perform forecast
-    forecast = model.forecast(steps=num_years * 12)
-    
-    # Create a new DataFrame for predictions with appropriate index
-    pred = pd.DataFrame(forecast, index=pred_index, columns=['Passengers_Stationary_2'])
+    # Make ARIMA forecast
+    ar_test_pred = ar.forecast(steps=20)
 
     # Plotting known and predicted values
     fig, ax = plt.subplots()
-    df['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='Known')
-    pred.plot(ax=ax, color='b', legend=True, label='Prediction')
-    plt.xlabel('Month')
+    df_stationary['Passengers_Stationary_2'].plot(style='--', color='gray', legend=True, label='Known')
+    ax.plot(ar_test_pred, color='b', label='Prediction')
+    plt.legend()
+    plt.xlabel('Time')
     plt.ylabel('Passengers_Stationary_2')
     st.pyplot(fig)
